7d71bdb87af27eabc19224eb177ac588f61f6b31
aheinrichs1 <112419161+aheinrichs1@users.noreply.github.com>
2022-11-02 13:31:06 -0500

    initial commit

---
+++ class_definitions/employee.py
@@ -0,0 +1,63 @@
+"""
+
+Program: employee.py
+Author: Alex Heirnichs
+Date created: 11/2/2022
+
+Contains a class called employee which demonstrates encapsulation
+"""
+import datetime
+
+
+class Employee:
+    """Employee Class"""
+
+    # Contructor
+    def __init__(self, lname, fname, addy, phone_number, salaried, start_date, salary):
+        self._last_name = lname
+        self._first_name = fname
+        self._address = addy
+        self._phone = phone_number
+        self._salaried = salaried
+        self._start_date = start_date
+        self._salary = salary
+
+    def display(self):
+        if self._salaried:
+            display_string = str(self._first_name) + " " + str(self._last_name) + "\n" \
+                             + self._address + "\nSalaried employee: $" \
+                             + str(self._salary) + "/year\nStart date: " \
+                             + self._start_date.strftime("%m-%d-%Y")
+        else:
+            display_string = str(self._first_name) + " " + str(self._last_name) + "\n" \
+                             + self._address + "\nHourly employee: $" \
+                             + str(self._salary) + "/hour\nStart date: " \
+                             + self._start_date.strftime("%m-%d-%Y")
+        return display_string
+
+    def __str__(self):
+        return "First name: " + self._first_name \
+                + "\nLast name: " + self._last_name \
+                + "\nAddress: " + self._address \
+                + "\nPhone number: " + self._phone \
+                + "\nSalaried: " + str(self._salaried) \
+                + "\nStart date: " + self._start_date.strftime("%m-%d-%Y") \
+                + "\nSalary: " + str(self._salary)
+
+    def __repr__(self):
+        return "First name: " + self._first_name \
+                + "\nLast name: " + self._last_name \
+                + "\nAddress: " + self._address \
+                + "\nPhone number: " + self._phone \
+                + "\nSalaried: " + str(self._salaried) \
+                + "\nStart date: " + self._start_date.strftime("%m-%d-%Y") \
+                + "\nSalary: " + str(self._salary)
+
+
+if __name__ == "__main__":
+    start = datetime.datetime(2018, 5, 15)
+    emp = Employee("Heinrichs", "Alex", "317 East St.\nAmes, Iowa",
+                   "641-691-9494", False, start, 12.25)
+    print(emp.display())
+    print(emp)
+    del emp
